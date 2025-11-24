dd3c9b0ba2ef8f1522829d378dfe51ab8bd1849a
0411kimyeonjun-jpg <0411kimyeonjun@gmail.com>
2025-11-24 16:55:09 +0900

    for문

---
+++ 구구단/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace 구구단
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int q = 1; q <= 9; q++)
+            {
+                Console.WriteLine($"\n {q} 단");
+
+                for (int w = 1; w <= 9; w++)
+                {
+                    int e = q * w;
+                    Console.WriteLine($"{q} x {w} = {e}");
+                }
+            }
+
+
+        }
+    }
+}
+
