9ec396dfd25d72e668bc06964f1e1e10af288d7b
0411kimyeonjun-jpg <0411kimyeonjun@gmail.com>
2025-11-24 15:47:52 +0900

    init

--- UnityStudy/Program.cs
+++ UnityStudy/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 반복문
+
+            // While
+            // dowhlie
+            // for
+
+            //int a = 0;
+
+            //while (a < 5) // 조건이 만족하면 실행
+            //{
+            //    Console.WriteLine("무한반복");
+            //    a++;
+            //}        
+
+            int num = 1;
+
+            while (num <= 10) 
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+
         }
+
     }
 }
