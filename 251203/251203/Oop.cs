96fbd718b8399321c12f106bb2322dfddc5d6901
김연준 <0411kimyeonjun@gmail.com>
2025-12-03 15:29:56 +0900

    Wizard

---
+++ 251203/251203/Oop.cs
@@ -0,0 +1,37 @@
+﻿namespace Program
+{
+    class Program
+    {
+
+        class Wizard
+        {
+            public int mp;
+            public int intelligence;
+
+
+            public Wizard()
+            {
+                mp = 50;
+                intelligence = 20;
+            }
+
+        }
+        // =======================================
+        // 1. Wizard 클래스를 만들어 다음 조건을 만족하세요:
+        // 
+        // - 멤버 변수: mp, intelligence (둘 다 int형)
+        // - 생성자에서 각각 50, 20으로 초기화
+        // - Main()에서 Wizard 객체를 생성하고 두 값을 출력
+        //
+        // [실행 결과]
+        // 마법사의 MP: 50, 지능: 20
+        // =======================================
+
+
+        static void Main()
+        {
+            Wizard wizard = new Wizard();
+            Console.WriteLine($"마법사의 MP: {wizard.mp}, 지능 : {wizard.intelligence}");
+        }
+    }
+}
